8c07905d2414040c66c7cf216308921f4fcaa798
saisai.chou <saisai.chou.u372@dirbato.co.jp>
2024-04-22 09:19:15 +0900

    最初のコミット

---
+++ techacademy_jdbc/src/dbSample/DbConnectSample01.java
@@ -0,0 +1,20 @@
+package dbSample;
+
+public class DbConnectSample01 {
+
+    public static void main(String[] args) {
+        //1. ドライバーのクラスをJava上で読み込む
+        
+        //2． DBと接続する
+        
+        //3. DBとやり取りする窓口（Statementオブジェクト）の作成
+        
+        //4,5.Select文の実行と結果の格納/代入
+       
+        //6.　結果を表示する
+        
+        //7.　接続を閉じる
+
+    }
+
+}
